8044c44ba231b8e1efc293dd4b25a497fe1a8971
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2019-07-18 17:38:49 +0800

    2

---
+++ NCaller/NCaller/ExtensionAPI/EntityExtension.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace NCaller
+{
+    public static class EntityExtension
+    {
+        public static CallerBase<T> Caller<T>(this T value)
+        {
+            var caller = (CallerBase<T>)SimpleCaller<T>.Create();
+            caller.SetInstance(value);
+            return caller;
+        }
+    }
+}
