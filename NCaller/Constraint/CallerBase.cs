19eab4fa1b1e92959a17be75356d42a519a745ff
Hu <16236903+NMSAzulX@users.noreply.github.com>
2019-07-20 21:58:35 +0800

    增加关键API注释

--- NCaller/Constraint/CallerBase.cs
+++ NCaller/Constraint/CallerBase.cs
@@ -7,8 +7,14 @@
             Instance = value;
         }
     }
+
+
+
+
     public abstract class CallerBase
     {
+
+
         public string _current_name;
         public CallerBase this[string name]
         {
@@ -17,16 +23,62 @@
                 return this;
             }
         }
+
+
+
+        /// <summary>
+        /// 如果不是静态类，可以新建一个实例
+        /// </summary>
         public virtual void New() { }
 
+
+
+
+        /// <summary>
+        /// 将字段或者属性作为动态调用返回
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public abstract CallerBase Get(string name);
 
+
+
+
+        /// <summary>
+        /// 通过指定泛型以及成员名反射出强类型的字段或者属性值
+        /// </summary>
+        /// <typeparam name="T">字段/属性的类型</typeparam>
+        /// <param name="name">字段/属性名</param>
+        /// <returns></returns>
         public abstract T Get<T>(string name);
 
+
+
+
+        /// <summary>
+        /// 通过索引名以及指定类型反射出强类型的字段或者属性值
+        /// </summary>
+        /// <typeparam name="T">字段/属性的类型</typeparam>
+        /// <returns></returns>
         public abstract T Get<T>();
 
+
+
+
+        /// <summary>
+        /// 通过指定泛型以及成员名设置字段或者属性值
+        /// </summary>
+        /// <param name="name">字段/属性名</param>
+        /// <param name="value">字段/属性新值</param>
         public abstract void Set(string name, object value);
 
+
+
+
+        /// <summary>
+        /// 通过索引设置字段或者属性值
+        /// </summary>
+        /// <param name="value">字段/属性新值</param>
         public abstract void Set(object value);
 
     }
