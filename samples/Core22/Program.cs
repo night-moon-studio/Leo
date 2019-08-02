4084737ff739992d67d6925452cf7b4957372ee0
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2019-08-02 15:07:38 +0800

    Update Program.cs

--- samples/Core22/Program.cs
+++ samples/Core22/Program.cs
@@ -21,7 +21,7 @@ namespace Core22
 
 
                 stopwatch.Restart();
-                for (int i = 0; i < 40000; i++)
+                for (int i = 0; i < 400000; i++)
                 {
                     var tEntity = new TestB();
                     if (tEntity.A2ge712 == "111")
@@ -37,7 +37,7 @@ namespace Core22
 
                 var entity = LinkOperator.Create(typeof(TestB));
                 stopwatch.Restart();
-                for (int i = 0; i < 40000; i++)
+                for (int i = 0; i < 400000; i++)
                 {
                     entity.New();
                     if (entity.Get<string>("A2ge712") == "111")
@@ -49,9 +49,22 @@ namespace Core22
                 stopwatch.Stop();
                 Console.WriteLine("NCaller SimpleCaller:\t" + stopwatch.Elapsed);
 
+                var dict = DictOperator.Create(typeof(TestB));
+                stopwatch.Restart();
+                for (int i = 0; i < 400000; i++)
+                {
+                    dict.New();
+                    if ((string)(dict["A2ge712"]) == "111")
+                    {
+                        //调用动态委托赋值
+                        dict["A2ge712"] = "222";
+                    }
+                }
+                stopwatch.Stop();
+                Console.WriteLine("NCaller DictCaller:\t" + stopwatch.Elapsed);
 
                 stopwatch.Restart();
-                for (int i = 0; i < 40000; i++)
+                for (int i = 0; i < 400000; i++)
                 {
                     RunDynamic(new TestB());
                 }
@@ -60,7 +73,7 @@ namespace Core22
 
 
                 stopwatch.Restart();
-                for (int i = 0; i < 40000; i++)
+                for (int i = 0; i < 400000; i++)
                 {
                     var tEntity = (new TestB()).LinkCaller();
                     if (tEntity.Get<string>("A2ge712") == "111")
@@ -75,7 +88,7 @@ namespace Core22
 
                 //entity = DynamicCaller.Create(typeof(TestB));
                 //stopwatch.Restart();
-                //for (int i = 0; i < 40000; i++)
+                //for (int i = 0; i < 400000; i++)
                 //{
                 //    entity.New();
                 //    if (entity.Get<DateTime>("Time") != TempTime)
@@ -88,7 +101,7 @@ namespace Core22
                 //Console.WriteLine("NCaller SimpleCaller:\t" + stopwatch.Elapsed);
 
                 //stopwatch.Restart();
-                //for (int i = 0; i < 40000; i++)
+                //for (int i = 0; i < 400000; i++)
                 //{
                 //    RunDynamicTime(new TestB());
                 //}
@@ -97,7 +110,7 @@ namespace Core22
 
                 //entity = DynamicCaller.Create(typeof(TestB));
                 //stopwatch.Restart();
-                //for (int i = 0; i < 40000; i++)
+                //for (int i = 0; i < 400000; i++)
                 //{
                 //    entity.New();
                 //    if (entity.Get<DateTime>("Time") != TempTime)
@@ -111,10 +124,10 @@ namespace Core22
                 Console.WriteLine("=========================================");
             }
 
-            var dict = DictOperator<TestB>.Create();
-            dict["Name"] = "Hello";
-            dict["Age"] = 100;
-            dict["Time"] = DateTime.Now;
+            //var dict = DictOperator<TestB>.Create();
+            //dict["Name"] = "Hello";
+            //dict["Age"] = 100;
+            //dict["Time"] = DateTime.Now;
 
             Console.ReadKey();
         }
