51c63333b6512598b7d6e26bd9c04519325ab783
alexinea <forerunner@outlook.com>
2020-11-09 00:36:26 +0800

    Add competitors benchmark

---
+++ test/BenchmarkTestForCompetitors/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarkTestForCompetitors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NatashaInitializer.InitializeAndPreheating();
+            BenchmarkRunner.Run<VisitorBenchmark>();
+            Console.ReadKey();
+        }
+    }
+}
