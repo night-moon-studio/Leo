4f27b749e177d42f0a775ed84d2b892581883d1d
alexinea <forerunner@outlook.com>
2020-11-11 00:03:53 +0800

    将 FindTreeType 与 AlgorithmType 合并，并改名为 AlgorithmKind

--- src/NMS.Leo.Typed/AlgorithmType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NMS.Leo.Typed
-{
-    public enum AlgorithmType
-    {
-        Precision,
-        Hash,
-        Fuzzy
-    }
-}
---
+++ src/NMS.Leo/AlgorithmType.cs
@@ -0,0 +1,12 @@
+﻿namespace NMS.Leo
+{
+    /// <summary>
+    /// Kind of algorithm to find tree
+    /// </summary>
+    public enum AlgorithmKind
+    {
+        Fuzzy,
+        Hash,
+        Precision
+    }
+}
